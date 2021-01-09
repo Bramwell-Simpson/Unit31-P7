b45904705f70fbf8b54e52c52667838fe86229ba
Bramwell-Simpson <32172769+Bramwell-Simpson@users.noreply.github.com>
2021-01-09 15:47:08 +0000

    remove npm animejs

---
+++ animations.js
@@ -0,0 +1,11 @@
+import anime from '/node_modules/animejs/lib/anime.es.js';
+
+anime ({
+    targets: '.serato-img-1',
+    opacity: 100,
+    duration: 3000,
+    delay: 3000,
+    endDelay: 5000,
+    direction: 'alternate',
+    easing: 'easeInOut'
+});
